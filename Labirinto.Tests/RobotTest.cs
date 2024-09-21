a2db2c0263b6416166a709f81eba43b97c29ff69
Matheus Felipe <matheus.felipescruz@gmail.com>
2024-09-20 23:16:02 -0300

    FEAT: Test Robot Orietantion

--- Labirinto.Tests/RobotTest.cs
+++ Labirinto.Tests/RobotTest.cs
@@ -20,5 +20,76 @@ namespace labirinto.Tests
             Assert.Equal(0, robot.battery);
         }
 
+        [Fact]
+        public void Sensor_ShouldReturnTrue()
+        {
+            string mazeTxt = "****\n*E *\n****";
+            Maze maze = new Maze(mazeTxt);
+            Robot robot = new Robot(maze);
+
+            Assert.True(robot.Sensor((1, 1), (1, 1)));
+        }
+
+        [Fact]
+        public void Sensor_ShouldReturnFalse()
+        {
+            string mazeTxt = "****\n*E *\n****";
+            Maze maze = new Maze(mazeTxt);
+            Robot robot = new Robot(maze);
+
+            Assert.False(robot.Sensor((1, 1), (1, 2)));
+        }
+
+        [Fact]
+        public void SetOrientation_ShouldReturnNorthOrientation()
+        {
+            string projectDirectory = Directory.GetCurrentDirectory();
+            string filePath = Path.Combine(projectDirectory, "..", "..", "..", "labirintos", "10x10normal.txt");
+            var data = File.ReadAllText(filePath);
+
+            Maze maze = new Maze(data);
+            Robot robot = new Robot(maze);
+
+            Assert.Equal("north", robot.compass);
+        }
+
+        [Fact]
+        public void SetOrientation_ShouldReturnEastOrientation()
+        {
+            string projectDirectory = Directory.GetCurrentDirectory();
+            string filePath = Path.Combine(projectDirectory, "..", "..", "..", "labirintos", "10x20semerro.txt");
+            var data = File.ReadAllText(filePath);
+
+            Maze maze = new Maze(data);
+            Robot robot = new Robot(maze);
+
+            Assert.Equal("east", robot.compass);
+        }
+
+        [Fact]
+        public void SetOrientation_ShouldReturnSouthOrientation()
+        {
+            string projectDirectory = Directory.GetCurrentDirectory();
+            string filePath = Path.Combine(projectDirectory, "..", "..", "..", "labirintos", "trivial.txt");
+            var data = File.ReadAllText(filePath);
+
+            Maze maze = new Maze(data);
+            Robot robot = new Robot(maze);
+
+            Assert.Equal("south", robot.compass);
+        }
+
+        [Fact]
+        public void SetOrientation_ShouldReturnWestOrientation()
+        {
+            string projectDirectory = Directory.GetCurrentDirectory();
+            string filePath = Path.Combine(projectDirectory, "..", "..", "..", "labirintos", "7x7.txt");
+            var data = File.ReadAllText(filePath);
+
+            Maze maze = new Maze(data);
+            Robot robot = new Robot(maze);
+
+            Assert.Equal("west", robot.compass);
+        }
     }
 }
